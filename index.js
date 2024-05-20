87c31039b800b635dd241a11891e279984759e30
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-05-19 23:53:47 -0500

    Update Serverless Email Phone

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const { contentEmail } = require("./template/template");
 
 require('dotenv').config();
 
+var AWS = require('aws-sdk');
+const sns = new AWS.SNS({ apiVersion: '2010-03-31' })
+
 app.use(express.json());
 
 app.post("/", async (req, res, next) => {
@@ -49,6 +52,55 @@ app.post("/", async (req, res, next) => {
       error: error.message
     });
   }
+});
+
+app.post('/sms', (req, res) => {
+  const { phone, subject } = req.body;
+
+  if (!from || !to || !subject) {
+    return res.status(400).json({
+      message: 'Se requieren las propiedades "phone", "subject" en el cuerpo de la solicitud.'
+    });
+  }
+
+  const params_sub = {
+    Protocol: 'sms', 
+    TopicArn: 'arn:aws:sns:us-east-1:946074075589:Momo', 
+    Endpoint: phone 
+  };
+
+ sns.subscribe(params_sub, (err, data) => {
+    if (err) {
+      console.error("Error al suscribirse al tema SNS:", err);
+      return res.status(500).json({
+        message: "Error al enviar el correo",
+        error: err
+      });
+    } else {
+
+      const params = {
+        Message: "Bienvenido a Momo Coffe, ingresando aqui puedes verificar tu cuenta.",
+        PhoneNumber: phone,
+        MessageAttributes: {
+          'AWS.SNS.SMS.SenderID': {
+            'DataType': 'String',
+            'StringValue': subject
+          }
+        }
+      };
+
+      var publishTextPromise = sns.publish(params).promise();
+
+      publishTextPromise.then(
+        function (data) {
+          res.end(JSON.stringify({ MessageID: data.MessageId }));
+        }).catch(
+          function (err) {
+            res.end(JSON.stringify({ Error: err }));
+          });
+
+    }
+  });
 });
 
 
