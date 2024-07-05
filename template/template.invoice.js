3d09d2498e61f7a3f9cd482f0f0f43292011adf3
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-07-05 17:48:04 -0500

    update email

--- template/template.invoice.js
+++ template/template.invoice.js
@@ -10,7 +10,7 @@ module.exports.contentEmailInvoice = (kiosko, restaurant, date_invoice, type_pay
 <body style="font-family: Arial, sans-serif; font-size: 14px; background: #f1f1f1; padding: 10px">
     <div style="margin: auto; width: 340px; background: #fff; border-radius: 8px; padding: 10px">
         <div style="text-align: center; padding-bottom: 10px;">
-            <img width="200" src="http://momo-coffee-prod-lb-1344034087.us-east-1.elb.amazonaws.com:83/assets/icons/momo_logo_email.png" alt="momo_coffe">
+            <img width="200" src="http://a59638a22dcac40f8954846bfa28d1e5-1443770537.us-east-1.elb.amazonaws.com:3003/assets/icons/momo_logo_email.png" alt="momo_coffe">
             <p style="font-size: 18px; font-weight: bold; margin:0px">MOMO Coffe</p>
             <p style="font-size: 16px; font-weight: bold; margin:0px">Café diferente para todos.</p>
             <p style="font-size: 16px; font-weight: bold; margin:0px">momocoffe.mx</p>
