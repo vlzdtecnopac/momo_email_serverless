4065065ad633482190ef88a8958794079d8c9b42
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-07-19 16:23:54 -0500

    Update App

--- template/template.invoice.js
+++ template/template.invoice.js
@@ -1,5 +1,5 @@
-module.exports.contentEmailInvoice = (id, kiosko, restaurant, date_invoice, type_payment, mount_discount, mount_propina, mount_sub_total, mount_total, line, type_discount, order_id,  payment_id, iva, table_id ) => { 
-    
+module.exports.contentEmailInvoice = (id, kiosko, restaurant, date_invoice, type_payment, mount_discount, mount_propina, mount_sub_total, mount_total, line, type_discount, order_id, payment_id, iva, table_id) => {
+
     let amountBeforeTax = [];
     let amountAfterTax = [];
 
@@ -22,50 +22,51 @@ module.exports.contentEmailInvoice = (id, kiosko, restaurant, date_invoice, type
             <p style="font-size: 16px; font-weight: bold; margin:0px"># ${id}</p>
             <p style="font-size: 16px; font-weight: bold; margin:0px">Ticket Mesa V${table_id}</p>
         </div>
-        <p style="text-align:center">------------------------------------------------------------------------</p>
-        <table style="width: 100%; text-align: left; border-collapse: collapse; display:block">
+        <p style='text-align:center' >----------------------------------------------------------------------</p>
+        <table style='width: 100%; text-align: left; border-collapse: collapse; display:block'>
             <tr>
-                <td style="width: 160px;">Pedido:</td>
+                <td style='width: 160px;'>Pedido:</td>
                 <td>${order_id}</td>
             </tr>
             <tr>
-                <td style="width: 160px;">Fecha:</td>
+                <td style='width: 160px;'>Fecha:</td>
                 <td>${date_invoice}</td>
             </tr>
             <tr>
-                <td style="width: 160px;">Restaurante:</td>
+                <td style='width: 160px;'>Restaurante:</td>
                 <td>${restaurant}</td>
             </tr>
             <tr>
-                <td style="width: 160px;">Kiosko:</td>
+                <td style='width: 160px;'>Kiosko:</td>
                 <td>${kiosko}</td>
             </tr>
         </table>
-        <table style="margin: 10px 0px; width: 100%; text-align: left; border-collapse: collapse">
+        <table style='margin: 10px 0px; width: 100%; text-align: left; border-collapse: collapse' >
             <tr>
-                <th style="width: 10%;">Cant</th>
-                <th style="width: 43%;">Producto</th>
-                <th style="width: 20%;">Iva</th>
-                <th style="width: 20%;">Precio Unit</th>
+                <th style='width: 10%;'>Cant</th>
+                <th style='width: 43%;'>Producto</th>
+                <th style='width: 20%;'>Iva</th>
+                <th style='width: 20%;'>Precio Unit</th>
             </tr>
             ${line.map(value => {
-                if(!value.isExtra){
-                    amountBeforeTax.push(value?.amountBeforeTax); 
-                    amountAfterTax.push(value?.amountAfterTax);
-                return `
-            <tr style="border-bottom: 1px solid #eee;">
-            <td style="width:20px">${value?.quantity}</td>
-            <td style="width:220px">${value?.productName}</td>
-            <td style="width:80px">${Number.isNaN(parseFloat(value?.tax[0]?.value?.toFixed(2)))? '' : parseFloat(value?.tax[0]?.value?.toFixed(2)) }</td>
-            <td style="width:80px">$ ${value?.unitPriceAfterTax.toFixed(2)}</td>
-        </tr>
+        if (!value.isExtra) {
+            amountBeforeTax.push(value?.amountBeforeTax);
+            amountAfterTax.push(value?.amountAfterTax);
+            return `
+            <tr style='border-bottom: 1px solid #eee;'>
+            <td style='width:20px'>${value?.quantity}</td>
+            <td style='width:220px'>${value?.productName}</td>
+            <td style='width:80px'>${Number.isNaN(parseFloat(value?.tax[0]?.value?.toFixed(2))) ? '' : parseFloat(value?.tax[0]?.value?.toFixed(2))}</td>
+            <td style='width:80px'>$ ${value?.unitPriceAfterTax.toFixed(2)}</td>
+            </tr>
             `
-        }})}
+        }
+    })}
         </table>
         <table style="width: 100%; line-height: inherit; text-align: left; border-collapse: collapse; display:block">
             <tr>
                 <td style="width: 240px">Medio de pago:</td>
-                <td>${type_payment == "effecty" ? "Efectivo" : "Tarjeta" }</td>
+                <td>${type_payment == "effecty" ? "Efectivo" : "Tarjeta"}</td>
             </tr>
             <tr>
                 <td>Sub-Total:</td>
@@ -94,7 +95,7 @@ module.exports.contentEmailInvoice = (id, kiosko, restaurant, date_invoice, type
             </tr>
             <tr>
                 <td style="width: 240px;">Base impuesto:</td>
-                <td>$ ${ amountBeforeTax.reduce((accumulator, currentValue) => accumulator + currentValue, 0).toFixed(2)}</td>
+                <td>$ ${amountBeforeTax.reduce((accumulator, currentValue) => accumulator + currentValue, 0).toFixed(2)}</td>
             </tr>
             <tr>
                 <td style="width: 240px;">Iva 16%</td>
@@ -102,7 +103,7 @@ module.exports.contentEmailInvoice = (id, kiosko, restaurant, date_invoice, type
             </tr> 
             <tr>
                 <td style="width: 240px;">Total c/impuesto:</td>
-                <td>$ ${ amountAfterTax.reduce((accumulator, currentValue) => accumulator + currentValue, 0).toFixed(2)}</td>
+                <td>$ ${amountAfterTax.reduce((accumulator, currentValue) => accumulator + currentValue, 0).toFixed(2)}</td>
             </tr>
         </table>
         <div style="text-align: center; padding-top: 20px; font-size: 12px;">
@@ -112,9 +113,9 @@ module.exports.contentEmailInvoice = (id, kiosko, restaurant, date_invoice, type
         <p style="text-align: center; font-size: 16px; margin: 0px">Puedes obtener tu factura aquí:</p>
         <a style="text-align: center; font-size: 16px; margin: 0px" href="https://autofactura-mx.toteat.com">https://autofactura-mx.toteat.com</a>
         <p style="text-align: center; font-size: 16px; margin: 0px">Disponible sólo en el mes del consumo.</p>
-        <p style="text-align: center; font-size: 14px;">#######################################</p>
+        <p style="text-align: center; font-size: 14px;">##################################</p>
         <p style="text-align: center; font-size: 14px;"><b>${order_id}/${payment_id}</b></p>
-        <p style="text-align: center; font-size: 14px;">#######################################</p>
+        <p style="text-align: center; font-size: 14px;">##################################</p>
         </div>
     </div>
 </body>
